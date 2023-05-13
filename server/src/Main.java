e2e933f3be4321ca35c95053ebe70ae5fb867a88
Sahan-Kavinda <101155746+Sahan-Kavinda@users.noreply.github.com>
2023-05-13 11:35:02 +0530

    first_commit

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            ServerSocket serverSocket = new ServerSocket(3000);//create server socket
+            Socket socket = serverSocket.accept();//create local socket
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            String massage = dataInputStream.readUTF();
+            System.out.println(massage);
+            socket.close();
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
